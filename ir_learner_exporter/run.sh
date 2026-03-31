2794dd1367d055e1340db4c87b30a4ee4680d798
infernalK <59821935+infernalK@users.noreply.github.com>
2026-03-31 16:14:48 +0200

    //

--- ir_learner_exporter/run.sh
+++ ir_learner_exporter/run.sh
@@ -5,8 +5,8 @@ export EXPORT_FILENAME="$(bashio::config 'export_filename')"
 export DEFAULT_MANUFACTURER="$(bashio::config 'default_manufacturer')"
 export DEFAULT_SUPPORTED_CONTROLLER="$(bashio::config 'default_supported_controller')"
 
-export PUBLIC_DIR="/config"
-export DATA_DIR="/data"
+export PUBLIC_DIR="/homeassistant/.storage"
+export DATA_DIR="/homeassistant"
 
 mkdir -p "$PUBLIC_DIR" "$DATA_DIR"
 
